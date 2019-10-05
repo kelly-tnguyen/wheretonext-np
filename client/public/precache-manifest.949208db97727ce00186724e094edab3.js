0a95f734a4ddb18ffdb0d77eddbea1909f5227d8
Kelly T Nguyen <Kelly.Nguyen@medspring.local>
2019-10-05 18:35:58 -0500

    added build files

---
+++ client/public/precache-manifest.949208db97727ce00186724e094edab3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126583a2f4b5d92ca692f7fb92843a0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64fc85e980554f6552a0",
+    "url": "/static/css/main.e02dbcad.chunk.css"
+  },
+  {
+    "revision": "0c5b3fc58e123f5e1dae",
+    "url": "/static/js/2.12dd86a5.chunk.js"
+  },
+  {
+    "revision": "64fc85e980554f6552a0",
+    "url": "/static/js/main.5cbf6f87.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  }
+]);
